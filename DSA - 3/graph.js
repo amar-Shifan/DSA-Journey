92fd7c3d12f12bc39e4202af5d10256378708878
amar-Shifan <amarshifaanvp@gmail.com>
2025-02-05 23:47:37 +0530

    graph implementation

--- DSA - 3/graph.js
+++ DSA - 3/graph.js
@@ -19,4 +19,84 @@
 // DAG (Directed Acyclic Graph) → No cycles; used in scheduling, dependencies.
 // Bipartite Graph → Vertices can be split into two groups with no internal connections.
 
-console.log('Graph')
+// Representation of Graph 
+// Adjacency Matrix
+let matrix = [
+    [0 , 1 ,0],
+    [1 , 0 ,1],
+    [0 , 1 ,0],
+]
+
+// console.log('adjacency Matrix : ' , matrix[0][0])
+
+// Adjacency List
+let list = {
+    'A' : ['B'],
+    'B' : ['A' , 'C'],
+    'C' : ['B'],
+}
+
+// console.log('Adjacency List : ' , list["A"])
+
+class Graph {
+    constructor(){
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = new Set()
+    }
+
+    addEdge(vertex1 , vertex2) {
+        if(!this.adjacencyList[vertex1]) {
+            this.addVertex(vertex1)
+        }
+        if(!this.adjacencyList[vertex2]) {
+            this.addVertex(vertex2)
+        }
+
+        this.adjacencyList[vertex1].add(vertex2)
+        this.adjacencyList[vertex2].add(vertex1)
+    }
+
+    hasEdge(vertex1 , vertex2) {
+        return (
+            this.adjacencyList[vertex1].has(vertex2) &&
+            this.adjacencyList[vertex2].has(vertex1) 
+        )
+    }
+
+    removeEdge(vertex1 , vertex2) {
+        this.adjacencyList[vertex1].delete(vertex2)
+        this.adjacencyList[vertex2].delete(vertex1)
+    }
+
+    removeVertex(vertex) {
+        if(!this.adjacencyList[vertex]) return null
+        for(let adjacentVertex of this.adjacencyList[vertex]) {
+            this.removeEdge(vertex , adjacentVertex);
+        }
+
+        delete this.adjacencyList[vertex]
+    }
+
+    display() {
+        for(let vertex in this.adjacencyList) {
+            console.log(vertex + '-->' + [...this.adjacencyList[vertex]])
+        }
+    }
+}
+
+let graph = new Graph()
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+
+graph.addEdge('A' , 'B')
+graph.addEdge('B' , 'C')
+graph.removeEdge('A' , 'B')
+graph.removeVertex('A')
+
+graph.display()
+// console.log('Edge Has : ' , graph.hasEdge('A' , 'B'))
+
